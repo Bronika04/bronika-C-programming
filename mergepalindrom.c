1c4b379344e794a3aee639d49bce92bfac9b8e1e
Bronika04 <160155036+Bronika04@users.noreply.github.com>
2024-02-22 18:19:30 +0530

    Add files via upload

---
+++ mergepalindrom.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char s1[50],s2[50];
+    printf("enter first the string");
+    fgets(s1,50,stdin);
+    printf("enter the second string");
+    fgets(s2,50,stdin);
+    
+
+    return 0;
+}
